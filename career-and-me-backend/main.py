cfeb4fe4c8765ade17922be2b3012e6bd03f3dc1
Michael Navazhylau <mankbf@umsystem.edu>
2022-04-10 02:50:23 -0500

    Career and Me, CORS and simple response

--- career-and-me-backend/main.py
+++ career-and-me-backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/pickhacks.db'
 db = SQLAlchemy(app)
+CORS(app)
 
 @app.route('/get-salary-numbers-length')
 def salary_count():
@@ -13,3 +16,11 @@ def salary_count():
     count = int(list(results)[0][0])
     
     return str(count)
+
+@app.route('/fields-of-study-list')
+def fields_of_study():
+    sql = text('select distinct "Field of Study" FROM "college-majors-by-field-of-study";')
+    results = db.engine.execute(sql)
+    fields_list = list(map(lambda result: result[0], list(results)))
+    print(fields_list)
+    return jsonify(fields_list)
