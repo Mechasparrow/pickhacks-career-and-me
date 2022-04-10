6d28c7acd38eb1ba10bcebb4b011cf64f3f9ac48
Michael Navazhylau <mankbf@umsystem.edu>
2022-04-10 03:45:27 -0500

    Dynamic form picking

--- pickhacks-career-viz/src/app/home-page/home-page.component.ts
+++ pickhacks-career-viz/src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {FormlyFieldConfig} from '@ngx-formly/core';
+import { CareerAndMeApiService } from 'src/services/career-and-me-api.service';
 
 @Component({
   selector: 'app-home-page',
@@ -9,48 +10,101 @@ import {FormlyFieldConfig} from '@ngx-formly/core';
 })
 export class HomePageComponent implements OnInit {
   states: String[] = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
-  fields_of_study: String[] = ['Agriculture','Architecture','Biology','Business','Communications','Communications technology','Computer and information technology','Construction','Culture and gender studies','Education','Engineering','Engineering technologies','English','Family and consumer sciences','Fine and performing arts','Foreign language','Healthcare and related','History','Interdisciplinary studies'];
-
+  
   form = new FormGroup({});
-  model = { state: 'Missouri', field_of_study: 'Architecture'};
-  fields: FormlyFieldConfig[] = [
-    {
-      key: 'state',
-      className: "FormField",
-      type: 'select',
-      templateOptions: {
-        label: 'Choose your State',
-        placeholder: 'Minnesota',
-        required: true,
-        options: this.states.map(state => {
-          return {value: state, label: state}
-        })
-      }
-    },
-    {
-      key: 'field_of_study',
-      className: "FormField",
-      type: 'select',
-      templateOptions: {
-        label: 'Choose your Field of Study',
-        placeholder: 'Architecture',
-        required: true,
-        options: this.fields_of_study.map(fields_of_study => {
-          return {value: fields_of_study, label: fields_of_study}
-        })
-      }
+  model = { state: 'Missouri', field_of_study: '' , major:''};
+
+  state_config: FormlyFieldConfig = {
+    key: 'state',
+    className: "FormField",
+    type: 'select',
+    templateOptions: {
+      label: 'Choose your State',
+      placeholder: 'Minnesota',
+      required: true,
+      options: this.states.map(state => {
+        return {value: state, label: state}
+      })
     }
+  };
+
+  field_of_study_config: FormlyFieldConfig = {
+    key: 'field_of_study',
+    className: "FormField",
+    type: 'select',
+    templateOptions: {
+      label: 'Choose your Field of Study',
+      placeholder: 'Architecture',
+      required: true,
+      options: []
+    },
+    hide: true
+  }
+
+  major_field_config: FormlyFieldConfig = {
+    key: 'major',
+    className: "FormField",
+    type: 'select',
+    templateOptions: {
+      label: 'Choose your Major',
+      placeholder: 'Comp Sci',
+      required: true,
+      options: []
+    },
+    hide: true
+  }
+
+  fields: FormlyFieldConfig[] = [
+    this.state_config,
+    this.field_of_study_config,
+    this.major_field_config
   ];
 
-  ngOnInit(): void {
-    
+  async ngOnInit(): Promise<void> {
+    await this.initFieldOfStudy();
+
+    this.form.valueChanges.subscribe(async value_change => {
+      let current_field: string = value_change["field_of_study"]
+
+      if (current_field != ''){  
+        await this.initMajor(current_field);
+      }
+
+    })
+  }
+
+  private async initMajor(current_field:string): Promise<void> {
+    let majors = await this.meApi.grabMajorFromFieldOfStudy(current_field);
+
+    this.major_field_config.templateOptions = {
+      label: 'Choose your Field of Study',
+      required: true,
+      options: majors.map(field => {return {value: field, label:field}})
+    };
+
+    this.major_field_config.hide = false
+  }
+
+  private async initFieldOfStudy(): Promise<void> {
+    let fields_of_study = await this.meApi.grabFieldOfStudy();
+
+    this.field_of_study_config.templateOptions = {
+      label: 'Choose your Field of Study',
+      required: true,
+      options: fields_of_study.map(field => {return {value: field, label:field}})
+    };
+
+    this.field_of_study_config.hide = false
+
   }
 
   onSubmit(model:any) {
     console.log(model);
   }
 
-  constructor() { }
+  constructor(private meApi: CareerAndMeApiService) { 
+
+  }
 
 
 }
--- pickhacks-career-viz/src/services/career-and-me-api.service.ts
+++ pickhacks-career-viz/src/services/career-and-me-api.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+import { firstValueFrom } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,4 +11,16 @@ export class CareerAndMeApiService {
   readonly BASE_URL:string = "http://localhost:5000";
 
   constructor(private http: HttpClient) { }
+
+  async grabFieldOfStudy() : Promise<string[]> {
+    let fields_of_study_promise = firstValueFrom(this.http.get(this.BASE_URL + "/fields-of-study-list"));
+
+    return <string[]>(await fields_of_study_promise);
+  }
+
+  async grabMajorFromFieldOfStudy(field_of_study:string): Promise<string[]> {
+    let major_list_promise = firstValueFrom(this.http.get(this.BASE_URL + "/majors-for-fields-of-study" + "?field=" + field_of_study));
+
+    return <string[]>(await major_list_promise);
+  }
 }
